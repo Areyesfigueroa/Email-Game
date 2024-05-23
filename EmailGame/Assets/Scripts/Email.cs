4b4fe3dcabeb9cfce4c59fe6b0b85821afbf187e
AKAMedia123 <46911752+AKAMedia123@users.noreply.github.com>
2024-05-22 20:26:55 -0700

    Email Data Pool
    Updated script to the email script to have sender/subject/body information.

--- EmailGame/Assets/Scripts/Email.cs
+++ EmailGame/Assets/Scripts/Email.cs
@@ -13,21 +13,34 @@ public class Email : MonoBehaviour
     public TMP_Text subjectText;
     public TMP_Text bodyText;
 
+
     // This is for example purposes only, will be part of a different script, the emailDataPool
     private readonly EmailData[] emails = {
         new EmailData("Me", "Sender", "This is a new subject email", "This is an email body"),
+        new EmailData("Me", "Boss", "I need to speak with you", "This is an email body"),
+        new EmailData("Me", "Wife", "Im in love with .......", "This is an email body"),
+        new EmailData("Me", "Prince Charles", "Save me by paying my ransom", "This is an email body"),
+        new EmailData("Me", "The Home Depot", "Here is your receipt", "This is an email body"),
+        new EmailData("Me", "Trek", "Your bike is due for its tire rotation", "This is an email body"),
+        new EmailData("Me", "Cousin Vinny", "I need a good lawyer", "This is an email body"),
+        new EmailData("Me", "Lover", "I LOVE YOU AND I DONT CARE WHO KNOWS IT", "This is an email body"),
+        new EmailData("Me", "Professor Rachel", "We need to talk about your daughters grades", "This is an email body"),
+        new EmailData("Me", "Matt Damon", "I won an Oscar and here's how you can too", "This is an email body"),
     };
+
+    public EmailData[] EmailPool { get { return emails; } }
 
 
     // Start is called before the first frame update
     void Start()
     {
+        int randomNum = UnityEngine.Random.Range(0, emails.Length - 1); // This will take from the list we created and pick a random message to appear
+        Debug.Log(randomNum);
 
-        int counter = transform.parent.GetComponent<EmailManager>().EmailCounter;
         // Temp, this is for showcase purposes. Illustrates how to dynamically change the email data.
-        fromText.text = emails[0].Sender + counter;
-        subjectText.text = emails[0].Subject + counter;
-        bodyText.text = emails[0].Body + counter;
+        fromText.text = emails[randomNum].Sender; //+ counter;
+        subjectText.text = emails[randomNum].Subject; //+ counter;
+        bodyText.text = emails[randomNum].Body; //+ counter;
         // profileIcon.GetComponent<SpriteRenderer>().sprite = personIcon2;
     }
 
